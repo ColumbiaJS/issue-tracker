2b10feb1fda116791d1c086e70e14f2d29b6d84a
Lev Brie <lev.brie@gmail.com>
2015-02-18 10:22:20 -0500

    feat(SidenavCtrl.js): add SidenavCtrl for handling conditional material sidenav menu

---
+++ public/app/layout/SidenavCtrl.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  angular
+    .module('app')
+    .controller('SidenavCtrl', SidenavCtrl);
+
+  function SidenavCtrl ($scope, $timeout, $mdSidenav, $log) {
+    $scope.close = function() {
+      $mdSidenav('left')
+        .close()
+        .then(function() {
+          $log.debug('close left is done');
+        });
+    };
+  }
+
+})();
