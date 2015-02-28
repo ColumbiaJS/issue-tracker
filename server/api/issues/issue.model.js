abe7e209c4c6ca9ecb2dc3588c135eeee8c4ca25
Lev Brie <lev.brie@gmail.com>
2015-02-28 02:36:28 -0500

    feat(issue.model): new issue schema fields, new static and pre-save hook
    * add title, user, created_at, updated_at, and closed_at (this is how
      Github represents times)
    * add statics.findMax to enable getting max number when creating a new
      issue
    * add pre-save hook to handle creating a new number for an issue if
      necessary, always update updated_at ts when saving

--- server/api/issues/issue.model.js
+++ server/api/issues/issue.model.js
@@ -14,11 +14,38 @@
     labels: [],
     milestone: {},
     number: Number,
+    title: String,
     body: String,
     state: { type: String, enum: STATES },
     author: {type: ObjectId, ref: 'User'},
     assignee: {type: ObjectId, ref: 'User'},
+    user: {},
+    created_at: Date,
+    updated_at: Date,
+    closed_at: Date,
     comments: []
+  });
+
+  IssueSchema.statics.findMax = function (callback) {
+    this.findOne({}) // 'this' now refers to the Member class
+      .sort({number: -1})
+      .limit(1)
+      .exec(callback);
+  };
+
+  IssueSchema.pre('save', function (next) {
+    var issue = this;
+    issue.updated_at = Date.now();
+    if (!issue.number) {
+      Issue.findMax(function (err, max) {
+        console.log('FOUND MAX');
+        console.log(max.number);
+        issue.number = max.number + 1;
+        next();
+      });
+    } else {
+      next();
+    }
   });
 
   Issue = mongoose.model('Issue', IssueSchema);
