498be4b82a66f066412b2bd1f63c12c997bfecaa
Lev Brie <lev.brie@gmail.com>
2015-02-28 02:40:02 -0500

    feat(issues.controller): flesh out issues api
    * add create and update methods with reasonably complete feature set

--- server/api/issues/issues.controller.js
+++ server/api/issues/issues.controller.js
@@ -26,15 +26,59 @@
       }
     },
     create: function (req, res) {
-
+      var newIssue = new Issue(req.body);
+      newIssue.created_at = Date.now();
+      newIssue.updated_at = Date.now();
+      newIssue.state = 'open';
+      newIssue.save(function (err, issue) {
+        if (err) { return errorHandler.error(res, err); }
+        console.log('saved issue');
+        console.log(issue);
+        return res.status(200).send(issue);
+      });
     },
     update: function (req, res) {
-
+      console.log('UPDATING ISSUE ON SERVER');
+      var id = req.params.id;
+      Issue.findOne({_id: id}).exec(updateIssue(req, res));
     },
     destroy: function (req, res) {
 
     }
   };
+
+  function updateIssue(req, res) {
+    return function (err, issue) {
+      if (err) { return errorHandler.error(res, err); }
+      if (!issue) { return res.status(404).send({reason: err.toString()}); }
+      var issueData = req.body;
+      issue.url = issueData.url;
+      issue.milestone = issueData.milestone;
+      issue.number = issueData.number;
+      issue.title = issueData.title;
+      issue.body = issueData.body;
+      issue.state = issueData.state;
+      issue.author = issueData.author;
+      issue.assignee = issueData.assignee;
+      issue.user = issueData.user;
+      issue.created_at = issueData.created_at;
+      issue.updated_at = issueData.updated_at;
+      issue.closed_at = issueData.closed_at;
+      // issue.comments = issueData.comments;
+      // issue.labels = issueData.labels;
+      issue.save(function (err, data) {
+        onIssueSave(req, res, err, data);
+      });
+
+    };
+  }
+
+  function onIssueSave (req, res, err, data) {
+    if(err) {
+      return errorHandler.error(res, err);
+    }
+    return res.status(200).send(data);
+  }
 
   module.exports = issuesController;
 
